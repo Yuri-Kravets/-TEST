94fa5941f1dc03d7629e81d02f80fa12691be29e
Yuri-Kravets <kravets.yuri.v@gmail.com>
2022-03-20 15:18:26 +0200

    update

--- js/script2.js
+++ js/script2.js
@@ -1,56 +1,57 @@
 "use strict";
 (function () {
-let out = document.getElementById('result');
-
-function task1() {
-    let i = 11;
-    while ( i <= 33) {
-        if(i === 11) {
-            out.innerHTML = i;
-        } else {
-            out.innerHTML= out.innerHTML + '<br>' + i;          
-        }
-        i++;
-    }       
-};
-
-function task2(i,j) {
-    
-    let valueI = (i % 2 === 0)? i : i + 1;
-    
-    while ( i <= j) {
-        if(i % 2 === 0 ) {
-            if (i === valueI ) {
-                out.innerHTML = i;
-            } else {
-                out.innerHTML = out.innerHTML + '<br>' +  i;
-            }
-        }
-        i++;
-    }       
-};
+// let out = document.getElementById('result');
+//
+// function task1() {
+//     let i = 11;
+//     while ( i <= 33) {
+//         if(i === 11) {
+//             out.innerHTML = i;
+//         } else {
+//             out.innerHTML= out.innerHTML + '<br>' + i;
+//         }
+//         i++;
+//     }
+// };
+//
+// function task2(i,j) {
+//
+//     let valueI = (i % 2 === 0)? i : i + 1;
+//
+//     while ( i <= j) {
+//         if(i % 2 === 0 ) {
+//             if (i === valueI ) {
+//                 out.innerHTML = i;
+//             } else {
+//                 out.innerHTML = out.innerHTML + '<br>' +  i;
+//             }
+//         }
+//         i++;
+//     }
+// };
 
 function task3(n) {
-
+    let a = 1;
     let sum = 0;
 
-    while (sum <= n) {
-        sum += n;
+    while (a <= n) {
+        sum += a;
+        a++;
     }
-    sum++;
-    return sum;
+    console.log(sum)
 }
-    let lastNumber = 15;
-    let i = 1;
-    let sum = 0;
 
-    while(i <= lastNumber) {
-        sum += +i;
-        i++;
-    }
-    console.log(sum);
+    // let lastNumber = 15;
+    // let i = 1;
+    // let sum = 0;
+    //
+    // while(i <= lastNumber) {
+    //     sum += +i;
+    //     i++;
+    // }
+    // console.log(sum);
 
-console.log(task3(100))
+task3(10)
 function init() {
     task2(36,80);
 
